dde80b5dc1d5320227abb49aafd9258f97971648
kestrelcreek <will@willsimpson.org>
2022-12-21 21:52:10 -0800

    Search multiple file for tags

--- seachfiles.py
+++ seachfiles.py
@@ -3,3 +3,18 @@
 import os
 import glob
 import pathlib
+import re
+
+
+pattern= "(?<!#)#(?![#,'])[0-9,a-z,A-Z]*.|\[\[\D.*\]\]"
+targetDir="/Users/will/Dropbox/Projects/zettelkasten/testzks/Sample-Zettelkasten-Archive-main/"
+
+for file in os.listdir(targetDir):
+    with open(targetDir + file) as f:
+        data=f.read()
+        try:
+            print(file, re.findall(pattern, data))
+        except:
+            print("Search not found.")
+    print(" _____ \n")        
+        
