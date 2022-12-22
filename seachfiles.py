17afda1e2fa64cbff5c05376dedf4e6ce7394e94
kestrelcreek <will@willsimpson.org>
2022-12-22 12:37:14 -0800

    Final function

--- seachfiles.py
+++ seachfiles.py
@@ -3,15 +3,22 @@
 import os
 import re
 
-pattern= "(?<!#)#(?![#,'])[0-9,a-z,A-Z]*.|\[\[\D.*\]\]"
+# This function looks in a directory (targetDir) of files for the occurrence of tags (tagPattern).
+
+tagPattern= "(?<!#)#(?![#, ,'])[0-9,a-z,A-Z]*.|\[\[\D.*\]\]"
 targetDir="/Users/will/Dropbox/Projects/zettelkasten/testzks/Sample-Zettelkasten-Archive-main/"
 
+
 def findmulti(target, pattern):
+    lst=[]
     for file in os.listdir(target):
-        with open(target + file) as f:
+        with open(target + file) as f: 
             data=f.read()
-            print(file, re.findall(pattern, data))
-        print(" _____ \n")        
+            tags=file, re.findall(pattern, data)
+            lst.append(tags)
+    return lst 
         
 if __name__ == "__main__":
-    findmulti(targetDir, tagPattern)
+    for i in findmulti(targetDir, tagPattern):
+       print(i)
+
