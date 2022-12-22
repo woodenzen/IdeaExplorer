5042be1eb1f99b4414e23f435f7935651c8ee442
kestrelcreek <will@willsimpson.org>
2022-12-22 12:57:11 -0800

    Final Format

--- seachfiles.py
+++ seachfiles.py
@@ -5,10 +5,6 @@ import re
 
 # This function looks in a directory (targetDir) of files for the occurrence of tags (tagPattern).
 
-tagPattern= "(?<!#)#(?![#, ,'])[0-9,a-z,A-Z]*.|\[\[\D.*\]\]"
-targetDir="/Users/will/Dropbox/Projects/zettelkasten/testzks/Sample-Zettelkasten-Archive-main/"
-
-
 def findmulti(target, pattern):
     lst=[]
     for file in os.listdir(target):
@@ -19,6 +15,8 @@ def findmulti(target, pattern):
     return lst 
         
 if __name__ == "__main__":
+    tagPattern= "(?<!#)#(?![#, ,'])[0-9,a-z,A-Z]*.|\[\[\D.*\]\]"
+    targetDir="/Users/will/Dropbox/Projects/zettelkasten/testzks/Sample-Zettelkasten-Archive-main/"
     for i in findmulti(targetDir, tagPattern):
        print(i)
 
