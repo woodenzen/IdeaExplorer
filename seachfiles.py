0c71079b1f7c02c299d625e830c016b374b89b3e
kestrelcreek <will@willsimpson.org>
2022-12-22 12:20:49 -0800

    stash pop

--- seachfiles.py
+++ seachfiles.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 
 import os
-import glob
-import pathlib
 import re
-this is a test.
 
-pattern= "(?<!#)#(?![#,'])[0-9,a-z,A-Z]*.|\[\[\D.*\]\]"
+
+tagPattern= "(?<!#)#(?![#, ,'])[0-9,a-z,A-Z]*.|\[\[\D.*\]\]"
 targetDir="/Users/will/Dropbox/Projects/zettelkasten/testzks/Sample-Zettelkasten-Archive-main/"
 
-for file in os.listdir(targetDir):
-    with open(targetDir + file) as f:
-        data=f.read()
-        try:
+def findmulti(target, pattern):
+    for file in os.listdir(target):
+        with open(target + file) as f:
+            data=f.read()
             print(file, re.findall(pattern, data))
-        except:
-            print("Search not found.")
-    print(" _____ \n")        
+        print(" _____ \n")        
         
+if __name__ == "__main__":
+    findmulti(targetDir, tagPattern)
